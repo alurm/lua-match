82ef94df45d20a9621883728fb51909c47df4a62
Alan Urmancheev <108410815+alurm@users.noreply.github.com>
2024-12-30 11:55:14 +0400

    Update tag.lua

--- tag.lua
+++ tag.lua
@@ -1,12 +1,12 @@
-return function(tag, object)
-  setmetatable(object, {
-    __call = function(object, switch)
-      local f = switch[tag]
-      if f then return f(object) end
+return function(tag, value)
+  setmetatable(value, {
+    __call = function(value, switch)
+      local case = switch[tag]
+      if case then return case(value) end
       local default = switch[false]
       if default then return default() end
       error('no match for tag ' .. tag)
     end
   })
-  return object
+  return value
 end
