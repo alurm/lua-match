d95cbb7b074e061bd912850955b210a7b8524827
Alan Urmancheev <alan.urman@gmail.com>
2024-12-31 06:21:31 +0400

    Square -> rectangle; edit README.md

--- README.md
+++ README.md
@@ -6,17 +6,20 @@ A minimal usage example:
 
 ```lua
 local tag = dofile'tag.lua'
-local a_square = tag('square', { width = 10, height = 20 })
+local a_rectangle = tag('rectangle', { width = 10, height = 20 })
 
 local switch = {
-  square = function(s) return print('Got a square shape with the width of ' .. s.width) end,
+  rectangle = function(s) return print('Got a rectangle shape with the width of ' .. s.width) end,
   -- An optional default case.
   [false] = function() return print('Shape type is not known') end,
 }
 
-a_square(switch)
+-- Will print "Got a rectangle shape with the width of 10".
+a_rectangle(switch)
 ```
 
 For a more thorough example, see [example.lua](example.lua).
+
+Tags are implemented by setting the `__call` metamethod on a table.
 
 License: MIT.
--- example.lua
+++ example.lua
@@ -1,29 +1,29 @@
 local tag = dofile'tag.lua'
 
-local a_square = tag('square', { width = 10, height = 20 })
+local a_rectangle = tag('rectangle', { width = 10, height = 20 })
 local a_circle = tag('circle', { radius = 5 })
 local a_belochka = tag('squirrel', {})
 
 local function area_without_default(shape)
   return shape {
     circle = function(c) return 3.1415 * c.radius * c.radius end,
-    square = function(s) return s.height * s.width end,
+    rectangle = function(s) return s.height * s.width end,
   }
 end
 
 local function area_with_default(shape)
   return shape {
     circle = function(c) return 3.1415 * c.radius * c.radius end,
-    square = function(s) return s.height * s.width end,
+    rectangle = function(s) return s.height * s.width end,
     [false] = function() return 9999 end
   }
 end
 
-print(area_with_default(a_square))
+print(area_with_default(a_rectangle))
 print(area_with_default(a_circle))
 print(area_with_default(a_belochka))
 
-print(area_without_default(a_square))
+print(area_without_default(a_rectangle))
 print(area_without_default(a_circle))
 
 -- This will throw an error.
